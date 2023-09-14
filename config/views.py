fd2cc50de62014a094c97ec6a17739e4501d2a8e
김권아 <bo32123@naver.com>
2023-09-14 17:34:25 +0900

    test2

--- config/views.py
+++ config/views.py
@@ -28,3 +28,20 @@ def burger_search(request):
     }
 
     return render(request, "burger_search.html", context)
+
+
+def burger_search2(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+
+    else:
+        burgers = Burger.objects.none()
+
+    context = {
+        "burgers": burgers,
+    }
+
+    return render(request, "burger_search.html", context)
+
